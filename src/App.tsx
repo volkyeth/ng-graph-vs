f1247f1d326a757c22b8fe72466b4982836e105f
Volky <volkyeth@pm.me>
2024-02-28 12:12:19 -0300

    Remove AddNegationModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-
 import { useAtom } from "jotai";
 import { useEffect } from "react";
 import { CytoscapeComponent } from "./components/app/CytoscapeComponent";
-import { AddNegationModal } from "./components/ui/AddNegationModal";
 import { graphIdAtom } from "./graph/state";
 import { style } from "./graph/style";
 
-
-
 function App() {
-  const [graphId, setGraphId] = useAtom(graphIdAtom)
+  const [graphId, setGraphId] = useAtom(graphIdAtom);
   useEffect(() => {
-   if (!graphId) setGraphId(Math.random().toString(36).substring(7))
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    if (!graphId) setGraphId(Math.random().toString(36).substring(7));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="w-screen h-screen">
       <CytoscapeComponent style={style} />
-      <AddNegationModal />
     </div>
   );
 }
